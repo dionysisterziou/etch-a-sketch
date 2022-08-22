204275fbeb5d300eff9863cba5aab5ef13f0ec53
Dionysis <dionysisterzios@gmail.com>
2022-08-22 17:24:01 +0300

    Create hovering effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
 const container = document.querySelector('#container');
 
+// Create grid
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'item');
     div.textContent = i;
     container.appendChild(div);
 }
+
+const items = document.querySelectorAll('.item');
+
+// Change color to grid items when they're hovered
+items.forEach((item) => {
+    function hover() {
+        item.style.backgroundColor = 'black';
+    }
+
+    item.addEventListener('mouseover', hover);
+})
