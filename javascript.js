3adc4f502446ed0cdd2ca8374b8b1d0c1c0dd4be
Dionysis <dionysisterzios@gmail.com>
2022-08-25 20:20:15 +0300

    Refactor createGrid()

--- javascript.js
+++ javascript.js
@@ -1,33 +1,42 @@
 const container = document.querySelector('#container');
 const buttonCreate = document.querySelector('#buttonCreate');
 
-// Default grid size
-let size = 16;
+let size = 16; // Default grid size
+let exists = false;
+
+function calculateSize(size) {
+    for (let i = 1; i <= Math.pow(size, 2); i++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'item');
+        container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+        div.textContent = '';
+        container.appendChild(div);
+    }
+}
+
+function addHover() {
+    let items = document.querySelectorAll('.item');
+    
+    items.forEach((item) => {
+        function hover() {
+            item.style.backgroundColor = 'black';
+        }
+    
+        item.addEventListener('mouseover', hover);
+    })
+}
+
 
 function createGrid() {
-    function calculateSize(size) {
-        for (let i = 1; i <= Math.pow(size, 2); i++) {
-            const div = document.createElement('div');
-            div.setAttribute('class', 'item');
-            container.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
-            div.textContent = '';
-            container.appendChild(div);
-        }
+    if (exists === false) {
+        calculateSize(size);
+
+        exists = true;
+    } else {
+        removeGrid();
+        calculateSize(size);
     }
 
-    function addHover() {
-        let items = document.querySelectorAll('.item');
-        
-        items.forEach((item) => {
-            function hover() {
-                item.style.backgroundColor = 'black';
-            }
-        
-            item.addEventListener('mouseover', hover);
-        })
-    }
-
-    calculateSize(size);
     addHover();
 }
 
@@ -50,7 +59,6 @@ function promptFunction() {
         } while (size > 100);
     }
 
-    removeGrid();
     createGrid();
 }
 
